10b248c38c624e285a19482814f7f2e92b9b3908
Alex Slubsky <aslubsky@gmail.com>
2015-03-30 11:50:57 +0300

    fix for angular 1.3

--- build/bz.src.js
+++ build/bz.src.js
@@ -28998,40 +28998,43 @@ define('bz/directives/bzLoadingContainer',[
     });
 
 });
-define('bz/filters/translate',['bz/app'], function(app) {
+define('bz/filters/translate',['bz/app'], function (app) {
 
-    app.filter('translate', ['$rootScope', function($rootScope) {
+    app.filter('translate', ['$rootScope', '$http', function ($rootScope, $http) {
         var strings = {};
         var trTimers = {};
-        app.filter('translate', ['$rootScope', '$http', function ($rootScope, $http) {
-            return function (string) {
-                var translateBundle = $rootScope.$localeBundle || {};
-                if (window.bazalt.trackNotTranslated != undefined) {
-                    if($rootScope.$localeBundle && translateBundle[string] == undefined && strings[string] == undefined){
-                        strings[string] = string;
-                        //console.log('add', string);
-                        //console.trace();
-                        trTimers[string] = setTimeout(function(){
-                            //console.log('send', string);
-                            $http({
-                                url: '/api/rest.php/translates?action=save-with-translate',
-                                method: 'PUT',
-                                data: {
-                                    new_words: string
-                                }
-                            });
-                        }, 10000);
-                    }
-                    if($rootScope.$localeBundle && translateBundle[string] != undefined && strings[string] != undefined){
-                        //console.log('clear', string);
-                        delete strings[string];
-                        clearTimeout(trTimers[string]);
-                    }
-                }
 
-                return translateBundle[string] || string;
-            };
-        }]);
+        var translateFilter = function (string) {
+            var translateBundle = $rootScope.$localeBundle || {};
+            if (window.bazalt.trackNotTranslated != undefined) {
+                if ($rootScope.$localeBundle && translateBundle[string] == undefined && strings[string] == undefined) {
+                    strings[string] = string;
+                    //console.log('add', string);
+                    //console.trace();
+                    trTimers[string] = setTimeout(function () {
+                        //console.log('send', string);
+                        $http({
+                            url: '/api/rest.php/translates?action=save-with-translate',
+                            method: 'PUT',
+                            data: {
+                                new_words: string
+                            }
+                        });
+                    }, 10000);
+                }
+                if ($rootScope.$localeBundle && translateBundle[string] != undefined && strings[string] != undefined) {
+                    //console.log('clear', string);
+                    delete strings[string];
+                    clearTimeout(trTimers[string]);
+                }
+            }
+
+            return translateBundle[string] || string;
+        };
+
+        translateFilter.$stateful = true;
+
+        return translateFilter;
     }]);
 
 });
